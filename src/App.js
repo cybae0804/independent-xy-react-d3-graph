65d2417120e9e16475e932e24ff371a9a219f1a0
Cy Bae <cy.bae@toyota.com>
2020-12-03 15:48:20 -0800

    data shows up, zoom works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import React, { createRef } from 'react';
+import * as d3 from 'd3';
+
 import './App.css';
 import D3Graph from './D3Graph';
 
-function App() {
-  return (
-    <div className="App">
-      <D3Graph
-        lapsMinMax={[10, 100]}
-        deltaMinMax={[10, 100]}
-      />
-    </div>
-  );
-}
+export default class App extends React.Component {
+  graphRef = createRef();
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <D3Graph
+          ref={this.graphRef}
+          xDomain={[0, 100]}
+          yDomain={[0, 100]}
+        >
+          {(xScale, yScale) => { 
+            const line = d3.line()
+              .x((d) => xScale?.(d.lap))
+              .y((d) => yScale?.(d.value));
+
+            return (
+              <path
+                key="main"
+                d={line([
+                  { lap: 1, value: 10 },
+                  { lap: 30, value: 50 },
+                  { lap: 50, value: 36 },
+                  { lap: 60, value: 23 },
+                  { lap: 80, value: 56 },
+                  { lap: 90, value: 67 },
+                ])}
+                stroke="black"
+                fill="none"
+                strokeWidth={1}
+              />
+            );
+          }}
+        </D3Graph>
+
+        <button
+          onClick={() => this.graphRef.current?.zoomToX([30, 50])}
+        > TEXT </button>
+      </div>
+    );
+  }
+}
--- src/D3Graph.js
+++ src/D3Graph.js
@@ -1,80 +1,168 @@
-import React, { useState } from 'react';
+import React, { createRef } from 'react';
 import * as d3 from 'd3';
-import classnames from 'classnames';
+import equal from 'fast-deep-equal/react';
 
-import Axis from './Axis';
-
-const margin = {
-  left: 40,
+const margins = {
+  left: 30,
   right: 15,
   top: 15,
   bottom: 15,
 };
 
-const clipPathId = 'hello';
-
-export const D3Graph = (props) => {
-  const [size, setSize] = useState({ width: 1000, height: 800 });
-  const [trans, setTrans] = useState(null);
-
-  let xScale = d3.scaleLinear().domain(props.lapsMinMax).range([margin.left, size.width - margin.right]);
-  let yScale = d3.scaleLinear().domain(props.deltaMinMax).range([size.height - margin.bottom, margin.top]);
-
-  if (trans) {
-    xScale = trans.rescaleX(xScale.domain(props.lapsMinMax));
-    yScale = trans.rescaleY(yScale.domain(props.deltaMinMax));
-  }
-
-  const line = d3.line()
-    .defined((d: any) => d.value !== null)
-    .x((d: any) => xScale(d.lap))
-    .y((d: any) => yScale(d.value));
-
-  const updateChart = (e) => {
-    setTrans(e.transform);
-  };
-
-  const handleZoom = d3.zoom()
-    .scaleExtent([1, 20])
-    .translateExtent([[margin.left, margin.top], [size.width - margin.right, size.height - margin.bottom]])
-    .extent([[margin.left, margin.top], [size.width - margin.right, size.height - margin.bottom]])
-    .on('zoom', updateChart);
-
-  const handleResize = (entries: IResizeEntry[]) => {
-    const { width, height } = entries?.[0]?.contentRect;
-    setSize({ width, height });
-  };
-
-  return (
-    <svg
-      style={{
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-      }}
-      ref={(ref: any) => {
-        if (ref) handleZoom(d3.select(ref));
-      }}
-    >
-      <Axis
-        axisCreator={d3.axisBottom(xScale)}
-        transform={`translate(0, ${size.height - margin.bottom})`}
-      />
-
-      <Axis
-        axisCreator={d3.axisLeft(yScale)}
-        transform={`translate(${margin.left}, 0)`}
-      />
-
-      <clipPath id={clipPathId}>
-        <rect
-          width={size.width - margin.left - margin.right}
-          height={size.height - margin.top - margin.bottom}
-          transform={`translate(${margin.left},${margin.top})`}
-        />
-      </clipPath>
-    </svg>
-  );
+const size = {
+  width: 500,
+  height: 500,
 };
 
-export default D3Graph;
+export default class D3Graph extends React.Component {
+  state = {
+    elements: null,
+  };
+
+  svgRef = createRef();
+  xAxisRef = createRef();
+  yAxisRef = createRef();
+  dataRef = createRef();
+
+  gx = () => d3.select(this.xAxisRef.current);
+  gy = () => d3.select(this.yAxisRef.current);
+
+  componentDidMount() {
+    this.tx = () => d3.zoomTransform(this.gx().node());
+    this.ty = () => d3.zoomTransform(this.gy().node());
+
+    this.initScales();
+    this.initAxes();
+    this.initZoom();
+    this.initData();
+
+    this.redraw();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!equal(prevState.elements, this.state.elements)) {
+      this.redraw();
+    }
+  }
+
+  initScales() {
+    this.x = d3.scaleLinear()
+      .domain(this.props.xDomain)
+      .range([margins.left, size.width - margins.right]);
+    this.y = d3.scaleLinear()
+      .domain(this.props.yDomain)
+      .range([size.height - margins.bottom, margins.top]);
+  }
+
+  initAxes() {
+    this.xAxis = (g, scale) => g
+      .attr("transform", `translate(0,${this.y(0)})`)
+      .call(d3.axisBottom(scale));
+
+    this.yAxis = (g, scale) => g
+      .attr("transform", `translate(${this.x(0)},0)`)
+      .call(d3.axisLeft(scale));
+  }
+
+  initZoom() {
+    this.z = d3.zoomIdentity;
+
+    this.zoomX = d3.zoom()
+      .scaleExtent([1, 10])
+      .translateExtent([[margins.left, margins.top], [size.width - margins.right, size.height - margins.bottom]])
+      .extent([[margins.left, margins.top], [size.width - margins.right, size.height - margins.bottom]]);
+    this.zoomY = d3.zoom()
+      .scaleExtent([1, 10])
+      .translateExtent([[margins.left, margins.top], [size.width - margins.right, size.height - margins.bottom]])
+      .extent([[margins.left, margins.top], [size.width - margins.right, size.height - margins.bottom]]);
+
+    this.zoom = d3.zoom().on("zoom", (e) => {
+      const t = e.transform;
+      const k = t.k / this.z.k;
+      const point = e.sourceEvent ? d3.pointer(e) : [size.width / 2, size.height / 2];
+
+      // is it on an axis?
+      const doX = point[0] > this.x.range()[0];
+      const doY = point[1] < this.y.range()[0];
+
+      if (k === 1) {
+        // pure translation?
+        doX && this.gx().call(this.zoomX.translateBy, (t.x - this.z.x) / this.tx().k, 0);
+        doY && this.gy().call(this.zoomY.translateBy, 0, (t.y - this.z.y) / this.ty().k);
+      } else {
+        // if not, we're zooming on a fixed point
+        doX && this.gx().call(this.zoomX.scaleBy, k, point);
+        doY && this.gy().call(this.zoomY.scaleBy, k, point);
+      }
+
+      this.z = t;
+
+      this.redraw();
+    });
+
+    this.gx().call(this.zoomX).attr("pointer-events", "none");
+    this.gy().call(this.zoomY).attr("pointer-events", "none");
+
+    d3.select(this.svgRef.current)
+      .call(this.zoom)
+      .call(this.zoom.transform, d3.zoomIdentity.scale(1));
+  }
+
+  initData() {
+    this.setState({ elements: this.props.children(this.x, this.y) });
+  }
+
+  redraw() {
+    const xr = this.tx().rescaleX(this.x);
+    const yr = this.ty().rescaleY(this.y);
+
+    this.gx().call(this.xAxis, xr);
+    this.gy().call(this.yAxis, yr);
+
+    this.setState({ elements: this.props.children(xr, yr) })
+  }
+
+  zoomToX(domain) {
+    if (domain[1] < domain[0]) return;
+    const d = [
+      domain[0] < this.props.xDomain[0] ? this.props.xDomain[0] : domain[0],
+      domain[1] > this.props.xDomain[1] ? this.props.xDomain[1] : domain[1],
+    ];
+
+    const scale = (size.width - margins.left - margins.right) / (this.x(d[1]) - this.x(d[0]))
+    this.gx().call(this.zoomX)
+      .call(this.zoomX.transform, d3.zoomIdentity
+        .scale(scale)
+        .translate(-this.x(d[0]) + (margins.left / scale), 0));
+
+    this.redraw();
+  }
+
+  zoomToY(domain) {
+    if (domain[1] < domain[0]) return;
+    const d = [
+      domain[0] < this.props.yDomain[0] ? this.props.yDomain[0] : domain[0],
+      domain[1] > this.props.yDomain[1] ? this.props.yDomain[1] : domain[1],
+    ];
+
+    const scale = (size.height - margins.top - margins.bottom) / (this.y(d[1]) - this.y(d[0]))
+    this.gx().call(this.zoomX)
+      .call(this.zoomY.transform, d3.zoomIdentity
+        .scale(scale)
+        .translate(-this.y(d[0]) + (margins.left / scale), 0));
+
+    this.redraw();
+  }
+
+  render() {
+    return (
+      <svg ref={this.svgRef} style={{ width: '100%', height: '100%' }}>
+        <g ref={this.xAxisRef} />
+        <g ref={this.yAxisRef} />
+        <g ref={this.dataRef}>
+          {this.state.elements}
+        </g>
+      </svg>
+    );
+  }
+}
